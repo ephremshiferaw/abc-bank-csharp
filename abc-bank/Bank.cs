f69d7dc9dc88ee9ef607570a9c404981e50b880b
ephremshiferaw <ephrem.shiferaw@brivo.com>
2015-03-08 15:33:43 -0400

    Refactoring to remove unused calls

--- abc-bank/Bank.cs
+++ abc-bank/Bank.cs
@@ -1,58 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace abc_bank
 {
     public class Bank
     {
-        private List<Customer> customers;
+        private readonly List<Customer> _customers;
 
         public Bank()
         {
-            customers = new List<Customer>();
+            _customers = new List<Customer>();
         }
 
         public void AddCustomer(Customer customer)
         {
-            customers.Add(customer);
+
+            if (customer == null)
+                throw new ArgumentNullException("customer", "Customer was not provided");
+            _customers.Add(customer);
         }
 
-        public String CustomerSummary() {
-            String summary = "Customer Summary";
-            foreach (Customer c in customers)
-                summary += "\n - " + c.GetName() + " (" + format(c.GetNumberOfAccounts(), "account") + ")";
-            return summary;
+        public String CustomerSummary()
+        {
+            return _customers.Aggregate("Customer Summary",
+                (current, c) =>
+                    current + ("\n - " + c.GetName() + " (" + Format(c.GetNumberOfAccounts(), "account") + ")"));
         }
 
-        //Make sure correct plural of word is created based on the number passed in:
-        //If number passed in is 1 just return the word otherwise add an 's' at the end
-        private String format(int number, String word)
+        /// <summary>
+        ///Make sure correct plural of word is created based on the number passed in:
+        ///If number passed in is 1 just return the word otherwise add an 's' at the end
+        /// </summary>
+        /// <param name="number">constion number</param>
+        /// <param name="word">word to format</param>
+        /// <returns>formated string</returns>
+        private static String Format(int number, String word)
         {
             return number + " " + (number == 1 ? word : word + "s");
         }
 
-        public double totalInterestPaid() {
-            double total = 0;
-            foreach(Customer c in customers)
-                total += c.TotalInterestEarned();
-            return total;
+        public double TotalInterestPaid()
+        {
+            return _customers.Sum(c => c.TotalInterestEarned());
         }
 
-        public String GetFirstCustomer()
-        {
-            try
-            {
-                customers = null;
-                return customers[0].GetName();
-            }
-            catch (Exception e)
-            {
-                Console.Write(e.StackTrace);
-                return "Error";
-            }
-        }
     }
 }
