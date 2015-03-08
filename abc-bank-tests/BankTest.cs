3089895f7eaa5dac6a3e272adaa87915c0b7de64
ephremshiferaw <ephrem.shiferaw@brivo.com>
2015-03-08 15:36:04 -0400

    minor refactoring to update code standards

--- abc-bank-tests/BankTest.cs
+++ abc-bank-tests/BankTest.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using abc_bank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace abc_bank_tests
 {
@@ -13,9 +13,9 @@ namespace abc_bank_tests
         [TestMethod]
         public void CustomerSummary() 
         {
-            Bank bank = new Bank();
-            Customer john = new Customer("John");
-            john.OpenAccount(new Account(Account.CHECKING));
+            var bank = new Bank();
+            var john = new Customer("John");
+            john.OpenAccount(new Account(Account.AccountType.Checking));
             bank.AddCustomer(john);
 
             Assert.AreEqual("Customer Summary\n - John (1 account)", bank.CustomerSummary());
@@ -23,36 +23,65 @@ namespace abc_bank_tests
 
         [TestMethod]
         public void CheckingAccount() {
-            Bank bank = new Bank();
-            Account checkingAccount = new Account(Account.CHECKING);
-            Customer bill = new Customer("Bill").OpenAccount(checkingAccount);
+            var bank = new Bank();
+            var checkingAccount = new Account(Account.AccountType.Checking);
+            var bill = new Customer("Bill").OpenAccount(checkingAccount);
             bank.AddCustomer(bill);
 
             checkingAccount.Deposit(100.0);
 
-            Assert.AreEqual(0.1, bank.totalInterestPaid(), DOUBLE_DELTA);
+            Assert.AreEqual(0.1, bank.TotalInterestPaid(), DOUBLE_DELTA);
         }
 
         [TestMethod]
         public void Savings_account() {
-            Bank bank = new Bank();
-            Account checkingAccount = new Account(Account.SAVINGS);
+            var bank = new Bank();
+            var checkingAccount = new Account(Account.AccountType.Savings);
             bank.AddCustomer(new Customer("Bill").OpenAccount(checkingAccount));
 
             checkingAccount.Deposit(1500.0);
 
-            Assert.AreEqual(2.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+            Assert.AreEqual(2.0, bank.TotalInterestPaid(), DOUBLE_DELTA);
         }
 
         [TestMethod]
         public void Maxi_savings_account() {
-            Bank bank = new Bank();
-            Account checkingAccount = new Account(Account.MAXI_SAVINGS);
+            var bank = new Bank();
+            var checkingAccount = new Account(Account.AccountType.MaxiSavingsOrignal);
             bank.AddCustomer(new Customer("Bill").OpenAccount(checkingAccount));
 
             checkingAccount.Deposit(3000.0);
 
-            Assert.AreEqual(170.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+            Assert.AreEqual(170.0, bank.TotalInterestPaid(), DOUBLE_DELTA);
+        }
+
+
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConnectTestTicksPerMillisecond()
+        {
+            var bank = new Bank();
+            bank.AddCustomer(null);
+        }
+
+
+
+
+        [TestMethod]
+        public void Null_TransactionDateTest()
+        {
+            var bank = new Bank();
+            var checkingAccount = new Account(Account.AccountType.Checking);
+            bank.AddCustomer(new Customer("Ephrem").OpenAccount(checkingAccount));
+
+            checkingAccount.Deposit(100.0);
+
+            Assert.AreEqual(0.1, bank.TotalInterestPaid(), DOUBLE_DELTA);
+
+            checkingAccount.Withdraw(100);
+
+            Assert.AreEqual(0.0, bank.TotalInterestPaid(), DOUBLE_DELTA);
         }
     }
 }
