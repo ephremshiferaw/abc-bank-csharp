5044f1a9b4d149caffea986f90c3a2929f5438b7
ephremshiferaw <ephrem.shiferaw@brivo.com>
2015-03-08 15:32:57 -0400

    Refactored for Transaction Updates, Added Account ID for Comparator, Refactoring to make class more readable, Updated MaxSaving logic pre the requirement

--- abc-bank/Account.cs
+++ abc-bank/Account.cs
@@ -1,84 +1,157 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace abc_bank
 {
-    public class Account
+    public class Account:IComparable<Account>
     {
 
-        public const int CHECKING = 0;
-        public const int SAVINGS = 1;
-        public const int MAXI_SAVINGS = 2;
-
-        private readonly int accountType;
-        public List<Transaction> transactions;
-
-        public Account(int accountType) 
+        public enum AccountType
         {
-            this.accountType = accountType;
-            this.transactions = new List<Transaction>();
+            [Description("Checking Account")] Checking = 0,
+
+            [Description("Savings Account")] Savings = 1,
+
+            [Description("Maxi Savings Account Orignal")] MaxiSavingsOrignal = 2,
+
+            [Description("Maxi Savings Account")] MaxiSavings = 3
         }
 
-        public void Deposit(double amount) 
+        private readonly AccountType _accountType;
+        private readonly List<Transaction> _transactions;
+
+        /// <summary>
+        /// No One out side should be able to add transactions with out calling the methods
+        /// </summary>
+        public ReadOnlyCollection<Transaction> Transactions
+        {
+            get { return _transactions.AsReadOnly(); }
+        }
+
+        public Guid AccountId { get; private set; }
+
+        public Account(AccountType accountType) 
+        {
+            _accountType = accountType;
+            _transactions = new List<Transaction>();
+            AccountId = Guid.NewGuid();//User this to unquily id an account
+        }
+
+     
+        /// <summary>
+        /// Overload needed to capture Transaction date
+        /// No requirement yet to Validate date, except of Null
+        /// TODO: What are the Date Validation Rules??
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="transactionDate"></param>
+        public void Deposit(double amount, DateTime? transactionDate=null)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("amount must be greater than zero");
+            }
+            _transactions.Add(transactionDate == null
+                ? new Transaction(amount,Transaction.TransactionTypes.Deposit)
+                : new Transaction(amount,transactionDate.Value,Transaction.TransactionTypes.Deposit));
+        }
+
+
+        /// <summary>
+        /// Overload needed to capture Transaction date
+        /// No requirement yet to Validate date, except of Null
+        /// TODO: What are the Date Validation Rules??
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="transactionDate"></param>
+        public void Withdraw(double amount, DateTime? transactionDate = null)
         {
             if (amount <= 0) {
                 throw new ArgumentException("amount must be greater than zero");
-            } else {
-                transactions.Add(new Transaction(amount));
             }
+
+             if (Balance() < amount) {
+                throw new ArgumentException("amount must not be more than balance");
+            }
+          
+           
+            _transactions.Add(transactionDate == null
+              ? new Transaction(-amount, Transaction.TransactionTypes.Withdraw)
+              : new Transaction(-amount, transactionDate.Value, Transaction.TransactionTypes.Withdraw));
         }
 
-        public void Withdraw(double amount) 
-        {
-            if (amount <= 0) {
-                throw new ArgumentException("amount must be greater than zero");
-            } else {
-                transactions.Add(new Transaction(-amount));
-            }
-        }
 
-        public double InterestEarned() 
+
+
+
+        public double InterestEarned()
         {
-            double amount = sumTransactions();
-            switch(accountType){
-                case SAVINGS:
+            var amount = SumTransactions();
+            switch (_accountType)
+            {
+                case AccountType.Savings:
                     if (amount <= 1000)
-                        return amount * 0.001;
-                    else
-                        return 1 + (amount-1000) * 0.002;
-    //            case SUPER_SAVINGS:
-    //                if (amount <= 4000)
-    //                    return 20;
-                case MAXI_SAVINGS:
+                        return amount*0.001;
+                    return 1 + (amount - 1000)*0.002;
+                case AccountType.MaxiSavingsOrignal:
                     if (amount <= 1000)
-                        return amount * 0.02;
+                        return amount*0.02;
                     if (amount <= 2000)
-                        return 20 + (amount-1000) * 0.05;
-                    return 70 + (amount-2000) * 0.1;
+                        return 20 + (amount - 1000)*0.05;
+                    return 70 + (amount - 2000)*0.1;
+                case AccountType.MaxiSavings:
+                    if (HasNoWithdrawalsInDays(10)) //5% assuming no withdrawals in the past 10 days 
+                        return amount * 0.05;
+                    return amount * 0.001; //otherwise 0.1%
                 default:
-                    return amount * 0.001;
+                    return amount*0.001;
             }
         }
 
-        public double sumTransactions() {
-           return CheckIfTransactionsExist(true);
-        }
 
-        private double CheckIfTransactionsExist(bool checkAll) 
+        private bool HasNoWithdrawalsInDays(int days)
         {
-            double amount = 0.0;
-            foreach (Transaction t in transactions)
-                amount += t.amount;
-            return amount;
+            var a = from transaction in _transactions
+                    where DateProvider.Instance.DaysSince(transaction.TransactionDate) <= days && transaction.TransactionType == Transaction.TransactionTypes.Withdraw
+                select transaction;
+            if (a.ToList().Count > 0) return false;
+                    return true;
         }
 
-        public int GetAccountType() 
+        public double Balance()
         {
-            return accountType;
+            return SumTransactions();
         }
 
+
+        public double SumTransactions() {
+            return _transactions.Sum(t => t.Amount);
+        }
+
+     
+        public AccountType GetAccountType() 
+        {
+            return _accountType;
+        }
+
+
+        public int CompareTo(Account other)
+        {
+            return AccountId == other.AccountId ? 1 : 0;
+        }
+
+        public override string ToString()
+        {
+            var fi = _accountType.GetType().GetField(_accountType.ToString());
+
+            var attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes.Length > 0 ? attributes[0].Description : _accountType.ToString();
+        }
     }
 }
